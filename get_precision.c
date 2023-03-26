bec9e0927a24c483a7336cc2e7e4af3b69c818ca
SuliatAbdulWakeel <suliatabdulwakeel@gmail.com>
2023-03-26 03:52:10 -0700

    printf

--- get_precision.c
+++ get_precision.c
@@ -8,6 +8,7 @@
  *
  * Return: Precision.
  */
+
 int get_precision(const char *format, int *i, va_list list)
 {
 	int curr_i = *i + 1;
@@ -34,8 +35,6 @@ int get_precision(const char *format, int *i, va_list list)
 		else
 			break;
 	}
-
 	*i = curr_i - 1;
-
 	return (precision);
 }
